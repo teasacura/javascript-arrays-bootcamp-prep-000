d5f583ff5111375b0585c2325816034c62bec5af
Eva Leake <teasacuraster@gmail.com>
2018-03-06 14:33:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,3 +38,8 @@ function removeElementFromBeginningOfArray(array) {
   newArray = array.slice(1)
   return newArray
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
