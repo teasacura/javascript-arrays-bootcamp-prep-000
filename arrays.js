404e0e667f8b6b1fbc07db08f5d1030e89a05994
Eva Leake <teasacuraster@gmail.com>
2018-03-06 14:15:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = [element, ...array]
+  array.unshift(element)
   return array
 }
 
